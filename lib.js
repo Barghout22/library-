127821f37598e72324ea084979b7abb74de46af5
Mahmoud <barghoutmahmoud2@gmail.com>
2022-08-15 17:21:02 +0200

    adjusted the remove button handler. put it inside of the display creater so it is reactivated every time the display is recreated. removed the seperate form page . put the form in a hidden div on the same index page that is shown when the new book button is pressed

--- lib.js
+++ lib.js
@@ -1,6 +1,13 @@
 let myLibrary=[];
 const bookCollection=document.querySelector('.bookShelf');
+const form=document.querySelector('.newBookForm');
+let removeBooks='';
 
+const harry=new Book('harry potter','jk',265,'read');
+const john=new Book('mikey','jojo',250,'read');
+const harr=new Book('alchemist','paolo',135,'unread');
+const joh=new Book('lord of the rings','tolkein',150,'read');
+const hary=new Book('welcome to the jungle','gnr',300,'read');
 
 
 
@@ -22,20 +29,25 @@ function displayMyBooks(myLibrary)
   for(let i=0;i<myLibrary.length;i++)
   {
     const mybook=document.createElement('div');
-    mybook.setAttribute('id',`${i}`);
+    mybook.setAttribute('id',`book${i}`);
     mybook.classList.add('book');
+
     const bookName=document.createElement('p');
     bookName.textContent=myLibrary[i].title;
     mybook.appendChild(bookName);
+
     const authorName=document.createElement('p');
     authorName.textContent=myLibrary[i].author;
     mybook.appendChild(authorName);
+    
     const pageNumber=document.createElement('p');
     pageNumber.textContent=myLibrary[i].pageNumber;
     mybook.appendChild(pageNumber);
+    
     const readStatus=document.createElement('button');
     readStatus.textContent='read status';
     mybook.appendChild(readStatus);
+    
     const removeBook=document.createElement('button');
     removeBook.textContent='remove book';
     removeBook.setAttribute('id',`${i}`);
@@ -46,7 +58,15 @@ function displayMyBooks(myLibrary)
     bookCollection.appendChild(mybook);
 
   }
-
+  removeBooks='';
+  removeBooks=document.querySelectorAll('.removeBook')
+  removeBooks.forEach(removeBook=>removeBook.addEventListener('click',()=>{
+    console.log(removeBook['id']);
+    myLibrary.splice(removeBook['id'],1);
+    clearMyBookSelf(bookCollection);
+    displayMyBooks(myLibrary);
+})
+);
 }
 
 function clearMyBookSelf(mybookSelf)
@@ -58,29 +78,18 @@ function clearMyBookSelf(mybookSelf)
 }
 
 
-const harry=new Book('harry potter','jk',265,'read');
-const john=new Book('mikey','jojo',250,'read');
-const harr=new Book('harry potter','jk',265,'read');
-const joh=new Book('mikey','jojo',250,'read');
-const hary=new Book('harry potter','jk',265,'read');
-const jon=new Book('mikey','jojo',250,'read');
-const hrry=new Book('harry potter','jk',265,'read');
-const jhn=new Book('mikey','jojo',250,'read');
 
 displayMyBooks(myLibrary);
 
 
 
+const addNewButton=document.querySelector('.addNewBook');
 
-//const submitButton=document.querySelector('#submitBook');
-const removeBooks=document.querySelectorAll('.removeBook');
-removeBooks.forEach(removeBook=>removeBook.addEventListener('click',()=>{
-    console.log(removeBook['id']);
-    myLibrary.splice(removeBook['id'],1);
-    console.log(myLibrary);
-   clearMyBookSelf(bookCollection);
-    displayMyBooks(myLibrary);
-})
+const submittNewBook=document.querySelector('#submittNewBook');
+
+
+
+addNewButton.addEventListener('click',()=>form.removeAttribute('hidden')
 );
 
-//submitButton.addEventListener('click',console.log("hello"));
+submittNewBook.addEventListener('click',()=>console.log(document.getElementById('.author-name')));
