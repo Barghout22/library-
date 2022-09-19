5fccba47ef62ee00f7fae8660c8149b9ef140905
Mahmoud <barghoutmahmoud2@gmail.com>
2022-09-19 16:08:34 +0200

    added a simple check of info validity the form displays a message asking the user to fill out all the fields before adding the book to the shelf

--- lib.js
+++ lib.js
@@ -1,9 +1,14 @@
-let myLibrary=[];
-const bookCollection=document.querySelector('.bookShelf');
-const form=document.querySelector('.newBookForm');
-let removeBooks='';
-let readStatus='';
-let placeholderObject={book:'',author:'',pages:'',readStatus:'not read'};
+let myLibrary = [];
+const bookCollection = document.querySelector(".bookShelf");
+const form = document.querySelector(".newBookForm");
+let removeBooks = "";
+let readStatus = "";
+let placeholderObject = {
+  book: "",
+  author: "",
+  pages: "",
+  readStatus: "not read",
+};
 
 /*const harry=new Book('harry potter','jk',265,'read');
 const john=new Book('mikey','jojo',250,'read');
@@ -12,152 +17,145 @@ const joh=new Book('lord of the rings','tolkein',150,'read');
 const hary=new Book('welcome to the jungle','gnr',300,'read');
 */
 
-
-class Book
-{constructor(title,author,pageNumber,readStatus)
-    {
-    this.title=title;
-    this.author=author;
-    this.pageNumber=pageNumber;
-    this.readStatus=readStatus;
-
-    }
-    
+class Book {
+  constructor(title, author, pageNumber, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pageNumber = pageNumber;
+    this.readStatus = readStatus;
+  }
 }
 
-function addBookToLibrary(book)
-{
-myLibrary.push(book);
-clearMyBookSelf(bookCollection);
-displayMyBooks(myLibrary);
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  clearMyBookSelf(bookCollection);
+  displayMyBooks(myLibrary);
 }
 
+function displayMyBooks(myLibrary) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    const mybook = document.createElement("div");
+    mybook.setAttribute("id", `book${i}`);
+    mybook.classList.add("book");
 
-function displayMyBooks(myLibrary)
-{
-  for(let i=0;i<myLibrary.length;i++)
-  {
-    const mybook=document.createElement('div');
-    mybook.setAttribute('id',`book${i}`);
-    mybook.classList.add('book');
-
-    const bookName=document.createElement('p');
-    bookName.textContent=myLibrary[i].title;
+    const bookName = document.createElement("p");
+    bookName.textContent = '"' + myLibrary[i].title + '"';
     mybook.appendChild(bookName);
 
-    const authorName=document.createElement('p');
-    authorName.textContent=myLibrary[i].author;
+    const authorName = document.createElement("p");
+    authorName.textContent = "by " + myLibrary[i].author;
     mybook.appendChild(authorName);
-    
-    const pageNumber=document.createElement('p');
-    pageNumber.textContent=myLibrary[i].pageNumber;
+
+    const pageNumber = document.createElement("p");
+    pageNumber.textContent = myLibrary[i].pageNumber +" pages";
     mybook.appendChild(pageNumber);
-    
-    const readStatus=document.createElement('button');
-    readStatus.textContent=myLibrary[i].readStatus;
-    if(myLibrary[i].readStatus==='read')
-    {
-        readStatus.classList.add('read'); 
-    }
-    else
-    {
-        readStatus.classList.add('notread'); 
+
+    const readStatus = document.createElement("button");
+    readStatus.textContent = myLibrary[i].readStatus;
+    if (myLibrary[i].readStatus === "read") {
+      readStatus.classList.add("read");
+    } else {
+      readStatus.classList.add("notread");
     }
 
-    readStatus.setAttribute('id',`${i}`);
-    readStatus.classList.add('readStatus');
+    readStatus.setAttribute("id", `${i}`);
+    readStatus.classList.add("readStatus");
     mybook.appendChild(readStatus);
-    
-    const removeBook=document.createElement('button');
-    removeBook.textContent='remove book';
-    removeBook.setAttribute('id',`${i}`);
-    removeBook.classList.add('removeBook');
+
+    const removeBook = document.createElement("button");
+    removeBook.textContent = "remove book";
+    removeBook.setAttribute("id", `${i}`);
+    removeBook.classList.add("removeBook");
     mybook.appendChild(removeBook);
 
-        
     bookCollection.appendChild(mybook);
-
   }
-    readStatus='';
-    readStatus=document.querySelectorAll('.readStatus');
-    readStatus.forEach(read=>read.addEventListener('click',()=>{
-    if(read.textContent==='read') 
-    {
-        read.classList.remove('read');
-        read.classList.add('notread'); 
-        read.textContent='not read';
-       
-    }
-    else
-    {
-        read.classList.remove('notread');
-        read.classList.add('read'); 
-        read.textContent='read';
-      
-    }
+  readStatus = "";
+  readStatus = document.querySelectorAll(".readStatus");
+  readStatus.forEach((read) =>
+    read.addEventListener("click", () => {
+      if (read.textContent === "read") {
+        read.classList.remove("read");
+        read.classList.add("notread");
+        read.textContent = "not read";
+      } else {
+        read.classList.remove("notread");
+        read.classList.add("read");
+        read.textContent = "read";
+      }
+    })
+  );
 
-
-    }));
-    
-    removeBooks='';
-    removeBooks=document.querySelectorAll('.removeBook')
-    removeBooks.forEach(removeBook=>removeBook.addEventListener('click',()=>{
-    myLibrary.splice(removeBook['id'],1);
-    clearMyBookSelf(bookCollection);
-    displayMyBooks(myLibrary);
-})
-);
+  removeBooks = "";
+  removeBooks = document.querySelectorAll(".removeBook");
+  removeBooks.forEach((removeBook) =>
+    removeBook.addEventListener("click", () => {
+      myLibrary.splice(removeBook["id"], 1);
+      clearMyBookSelf(bookCollection);
+      displayMyBooks(myLibrary);
+    })
+  );
 }
 
-function clearMyBookSelf(mybookSelf)
-{
-    while (mybookSelf.firstChild) {
-        mybookSelf.removeChild(mybookSelf.lastChild);
-    }
-
+function clearMyBookSelf(mybookSelf) {
+  while (mybookSelf.firstChild) {
+    mybookSelf.removeChild(mybookSelf.lastChild);
+  }
 }
 
+const addNewButton = document.querySelector(".addNewBook");
 
+const submittNewBook = document.querySelector("#submittNewBook");
 
-
-const addNewButton=document.querySelector('.addNewBook');
-
-const submittNewBook=document.querySelector('#submittNewBook');
-
-
-
-addNewButton.addEventListener('click',()=>{
-    form.removeAttribute('hidden');
-    document.getElementById('book_name').value='';
-    document.getElementById('author_name').value='';
-    document.getElementById('page_number').value='';
+addNewButton.addEventListener("click", () => {
+  form.removeAttribute("hidden");
+  document.getElementById("book_name").value = "";
+  document.getElementById("author_name").value = "";
+  document.getElementById("page_number").value = "";
 });
 
-submittNewBook.addEventListener('click',()=>{
-    addBookToLibrary(new Book(placeholderObject.book,placeholderObject.author, placeholderObject.pages,placeholderObject.readStatus));
-    form.setAttribute('hidden','true');}
+const errorSpan = document.querySelector(".errorMesage");
+submittNewBook.addEventListener("click", () => {
+  if (
+    !placeholderObject.book ||
+    !placeholderObject.author ||
+    !placeholderObject.pages ||
+    placeholderObject.book === " " ||
+    placeholderObject.author === " "
+  ) {
+    errorSpan.textContent = "please fill out all fields to continue";
+  } else {
+    addBookToLibrary(
+      new Book(
+        placeholderObject.book,
+        placeholderObject.author,
+        placeholderObject.pages,
+        placeholderObject.readStatus
+      )
+    );
+    form.setAttribute("hidden", "true");
+    errorSpan.textContent = " ";
+  }
+});
+
+let Inputs = document.querySelectorAll("input");
+Inputs.forEach((input) =>
+  input.addEventListener("blur", () => sortInputValue(input))
 );
 
-let Inputs=document.querySelectorAll('input');
-Inputs.forEach(input=>input.addEventListener('blur',()=>sortInputValue(input)));
-
-function sortInputValue(input)
-{
-  switch(input.name)
-  {
-    case 'book_name':
-        placeholderObject.book=input.value;
-    break;
-    case 'author_name':
-        placeholderObject.author=input.value;
-    break;
-    case 'page_number':
-        placeholderObject.pages=input.value;
-    break;
-    case 'status':
-        placeholderObject.readStatus=input.value;
-    break;
+function sortInputValue(input) {
+  switch (input.name) {
+    case "book_name":
+      placeholderObject.book = input.value;
+      break;
+    case "author_name":
+      placeholderObject.author = input.value;
+      break;
+    case "page_number":
+      placeholderObject.pages = input.value;
+      break;
+    case "status":
+      placeholderObject.readStatus = input.value;
+      break;
+  }
 }
-
-}
-
